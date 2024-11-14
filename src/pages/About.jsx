1e18c72cfb00dfb2056820739df897e1e81843e3
Rqmba <ramblack45@gmail.com>
2024-11-14 09:43:47 +0100

    Initial commit after reset

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <>
+        <h1 className="text-6xl mb-4">Github finder</h1>
+        <p className='mb-4 text-2xl font-light'>
+        Une application React pour rechercher des profils GitHub et voir les détails du profil.
+        </p>
+    </>
+  )
+}
+
+export default About
